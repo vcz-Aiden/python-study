9d3dd4934d17159b651f1870b83bc11a89e696b3
changeui99 <changeui99@gmail.com>
2021-03-21 10:41:43 +0900

    initial commit

---
+++ lambda.py
@@ -0,0 +1,31 @@
+#lambda : 함수명 없이 1회용으로 사용할 함수를 정의하는 방법.
+
+import functools
+
+
+import csv
+
+if __name__ == "__main__" :
+    #sorting example
+    strings = ['bob', 'charles', 'alexander3', 'teddy']
+    strings.sort(key=lambda x:len(x))
+
+    print(strings)
+
+
+    #filter example
+    nums = [1, 2, 3, 6, 8, 9]
+    nums = list(filter(lambda x:x%2==0, nums))
+
+    print(nums)
+
+
+
+    #map example
+    nums = [1, 2, 3, 6, 8, 9]
+    nums = list(map(lambda x:x**2, nums))
+
+    print(nums)
+
+
+
