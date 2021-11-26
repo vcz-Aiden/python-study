cdc1bad5181c9dc4d59fb5c901ec25755e94b1a4
박창의 <changeui99@gmail.com>
2021-11-26 20:29:58 +0900

    number fo friends

--- problem_set/baekjoon4195.py
+++ problem_set/baekjoon4195.py
@@ -1,26 +1,38 @@
 n = int(input())
 
-for i in range(n) :
+def find(x):
+    if (x == network[x]['root']):
+        return x
+    else :
+        root = find(network[x]['root'])
+        network[x]['root'] = root
+        return root
+
+def union(x, y):
+    root_x = find(x)
+    root_y = find(y)
+
+    if (root_x != root_y):
+        network[root_y]['root'] = x
+        network[root_x]['num'] += network[root_y]['num']
+
+for i in range(n):
+    network = dict()
     m = int(input())
-    result_dic = {}
+    for j in range(m):
+        f1, f2 = (input()).split(" ")
 
-    for j in range(m) :
-        names = input().split(" ")
-        try :
-            set1 = result_dic[names[0]]
-        except KeyError :
-            result_dic[names[0]] = set(names)
-            set1 = result_dic[names[0]]
+        if (f1 not in network.keys()):
+            network[f1] = {
+                'root': f1,
+                'num': 1
+            }
 
-        try :
-            set2 = result_dic[names[1]]
-        except KeyError :
-            result_dic[names[1]] = set(names)
-            set2 = result_dic[names[1]]
+        if (f2 not in network.keys()):
+            network[f2] = {
+                'root': f2,
+                'num': 1
+            }
 
-        total = set1 | set2
-
-        for name in list(total) :
-            result_dic[name] = total
-
-        print(len(total))
+        union(f1, f2)
+        print(network[find(f1)]['num'])
